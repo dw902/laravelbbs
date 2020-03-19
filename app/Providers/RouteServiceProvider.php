23d094080d91f4329b098aa7290b97d8de556bfd
p73 <root_it1990@163.com>
2020-03-19 19:42:11 +0800

    修复跳转链接

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -21,7 +21,8 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/home';
+//    此处更改为主页
+    public const HOME = '/';
 
     /**
      * Define your route model bindings, pattern filters, etc.
