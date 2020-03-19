b07b058410c045a5398119f0c1f0822a1649bbee
p73 <root_it1990@163.com>
2020-03-19 12:58:22 +0800

    静态文件浏览器缓存问题

--- webpack.mix.js
+++ webpack.mix.js
@@ -12,4 +12,5 @@ const mix = require('laravel-mix');
  */
 
 mix.js('resources/js/app.js', 'public/js')
-    .sass('resources/sass/app.scss', 'public/css');
+    .sass('resources/sass/app.scss', 'public/css').version();
+//上边的。version是我们增加的这样更改样式就直接有作用了
