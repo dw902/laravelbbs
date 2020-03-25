8d37c50eb1684a97adb8344dbde384e2c5eb24ba
p73 <root_it1990@163.com>
2020-03-25 19:14:27 +0800

    '分类列表'

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -30,6 +30,7 @@ class AppServiceProvider extends ServiceProvider
         \App\Models\User::observe(\App\Observers\UserObserver::class);
 		\App\Models\Reply::observe(\App\Observers\ReplyObserver::class);
 		\App\Models\Topic::observe(\App\Observers\TopicObserver::class);
+//		让数据不包裹data分页的时候要特殊处理
         Resource::withoutWrapping();
         //
     }
